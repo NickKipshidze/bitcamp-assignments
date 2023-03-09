cfd5154fad3100c34fa7a2cf4977bf1344a193cb
Nick Kipshidze <96648005+NickKipshidze@users.noreply.github.com>
2023-03-09 21:59:58 +0400

    Add files via upload

---
+++ 12-quotes.py
@@ -0,0 +1,8 @@
+# პირველი ვერსია
+print("What is the quote? These aren't the droids you're looking for.\nWho said it? Obi-Wan Kenobi\nObi-Wan Kenobi says, \"These aren't the droids \nyou're looking for.\"")
+
+# მეორე ვერსია
+print("""What is the quote? These aren't the droids you're looking for.
+Who said it? Obi-Wan Kenobi
+Obi-Wan Kenobi says, "These aren't the droids
+you're looking for." """)
