394350a08d2a097da6aeeeb5c1aaa7e3e318f89c
Nick Kipshidze <96648005+NickKipshidze@users.noreply.github.com>
2023-03-10 01:40:26 +0400

    Update 12-quotes.py

--- 12-quotes.py
+++ 12-quotes.py
@@ -1,3 +1,15 @@
 quote = input("What is the quote?") 
 author = input("Who said it?")
 print(f"{author} says, \"{quote}\"")
+
+# BONUS:
+
+quotes = {
+  "elon musk" : "AI is the most dangerous thing ever created.",
+  "bill gates" : "I use windows every day. I have at least 4 windows in my room.",
+  "steve jobs" : "My favorite kind of apple is mac book.",
+  "შოტა რუსთაველი" : "ლეკვი ლომისა სწორია, ძუ იყოს, თუმდა ხვადია.",
+  "oto zakalashvili" : "სწავლა და ბრძოლა!", 
+}
+
+print(f"{author} says, \"{quotes[author]}\"")
