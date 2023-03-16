5e232e44afb457f1d03a8feb6877f5af9fe9267b
Nick Kipshidze <96648005+NickKipshidze@users.noreply.github.com>
2023-03-16 15:06:55 +0400

    Update 20-grocery-list.py

--- 20-grocery-list.py
+++ 20-grocery-list.py
@@ -3,14 +3,11 @@ alphabet = [chr(i) for i in range(65, 65+26)]
 grocery_list = []
 
 while True:
-    item = input("").upper()
-
-    # I didnt understand control-d thing
-    # Doesnt work on my machine
-    if item == "DONE":
+    try:
+        item = input("").upper()
+        grocery_list.append(item)
+    except EOFError:
         break
-
-    grocery_list.append(item)
 
 list = list(dict.fromkeys(sorted(grocery_list)))
 
