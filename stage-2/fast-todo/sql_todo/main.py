203c60c39332e7ae7dfaea63541da67ac7270575
Nick Kipshidze <96648005+NickKipshidze@users.noreply.github.com>
2023-07-17 13:36:30 +0400

    New API using FastAPI, SQLAlchemy and Pydantic

---
+++ stage-2/fast-todo/sql_todo/main.py
@@ -0,0 +1,26 @@
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/tasks/", response_model=schemas.Task)
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    return crud.create_task(db=db, task=task)
+
+@app.get("/tasks/", response_model=list[schemas.Task])
+def read_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    tasks = crud.get_tasks(db, skip=skip, limit=limit)
+    return tasks
